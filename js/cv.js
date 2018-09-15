9846b16d9152b88beef6307d69d7efb1414f6681
Nikola <nikolabojkovic@gmail.comm>
2018-09-15 17:39:02 +0200

    download cv changed, download will start on button click

--- js/cv.js
+++ js/cv.js
@@ -1,45 +1,54 @@
 (function($) {
     "use strict"; // Start of use strict
-  var cv;
-   var apiUrl = "https://developer-tool.com/api/"
-// var apiUrl = "http://localhost:5000/api/"
 
-//   jQuery.getJSON("../data/cv.json", function (result) {
-//     cv = result;
-//   });
+  var apiUrl = "https://developer-tool.com/api/"
+  // var apiUrl = "http://localhost:5000/api/"
 
-  $.ajax({
-    url: apiUrl + "download",
-    method: "GET",
-    dataType: "json",
-    success: function (result) {
-        cv = result;
-        $("#download-cv").removeAttr('disabled');
-        $("#download-cv-text").removeClass('d-none');
-        $("#loading-cv").addClass('d-none');
-    },
-    error: function (error) {
-        console.log('error', error);
-    }
-  });
+  //   jQuery.getJSON("../data/cv.json", function (result) {
+  //     cv = result;
+  //   });
 
   $(document).on('click', '#download-cv',  function() {
-    if (cv === undefined) {
-        return;
-    }
+    // refresh UI
+    $("#download-cv").prop("disabled", true);
+    $("#download-cv-text").addClass('d-none');
+    $("#loading-cv").removeClass('d-none');
 
-    var blob = dataURItoBlob(cv.document);
-    var cvFile = new File([blob], cv.name, {type: cv.type});
+    // get data
+    $.ajax({
+      url: apiUrl + "download",
+      method: "GET",
+      dataType: "json",
+      success: function (cv) {
+          if (cv === undefined) {
+            return;
+          }
+    
+          var blob = dataURItoBlob(cv.document);
+          var cvFile = new File([blob], cv.name, {type: cv.type});
+      
+          //IE
+          if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+            window.navigator.msSaveBlob(cvFile, cvFile.name);
+            return;
+          } 
+      
+          var a = document.createElement("a");
+          var fileUrl = URL.createObjectURL(cvFile);   
+          a.href = fileUrl;
+          a.download = cvFile.name;
+          a.click();
 
-    //IE
-    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveBlob(cvFile, cvFile.name);
-      return;
-    } 
-
-    var fileUrl = URL.createObjectURL(cvFile);   
-    this.href = fileUrl;
-    this.download = cvFile.name;
+          // refresh UI
+          $("#loading-cv").addClass('d-none');
+          $("#download-cv").prop("disabled", false);
+          $("#download-cv-text").removeClass('d-none');
+          
+      },
+      error: function (error) {
+          alert("Server not available at the moment, pease try later.");
+      }
+    });
   });
 
   var dataURItoBlob = function (dataURI) {
