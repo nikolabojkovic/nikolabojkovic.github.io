b43cf5b823ea6f740e4ab29175a6569093b44fdc
Nikola <nikolabojkovic@gmail.comm>
2018-10-08 23:38:08 +0200

    skills badge and chart updated

--- js/development-charts.js
+++ js/development-charts.js
@@ -9,40 +9,35 @@ var myPieChart = new Chart(ctx,{
         datasets: [{
             data: [2.5, 1, 0.6],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.8)',
+                'rgba(20, 155, 32, 0.8)',
                 'rgba(54, 162, 235, 0.8)',
                 'rgba(255, 206, 86, 0.8)',
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
+                'rgba(255,255,255,1)',
+                'rgba(255,255,255,1)',
+                'rgba(255,255,255,1)',
             ],
             borderWidth: 1
         }],
     
         // These labels appear in the legend and in the tooltips when hovering different arcs
         labels: [
-            'Desktop app development',
-            'Web development',
-            'Mobile development'
+            'Desktop app',
+            'Web',
+            'Mobile'
         ]
     },
     options: {
-        title: {
-            display: true,
-            text: '4 years experience',
-            fontColor: '#FFF',
-            fontStyle: 'bold',
-            fontSize: 18
-        },
         legend: {
             display: true,
             labels: {
                 fontColor: 'rgb(255, 255, 255)'
             },
             position: 'top'
-        }
+        },
+        cutoutPercentage: 80,
+        animation: { animateScale: true }
     }
 });
 
