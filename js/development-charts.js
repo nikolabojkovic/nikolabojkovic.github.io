851a826ea80fbb218110fe7f0080b1c25d596de3
Nikola <nikolabojkovic@gmail.comm>
2020-01-20 22:04:45 +0100

    dev stack chart added

--- js/development-charts.js
+++ js/development-charts.js
@@ -1,19 +1,20 @@
 var myPieChart;
+var linearChart;
 
 $(document).ready(function() {
 
 });
 
 function createChart() {
-    var yearsOfExperiance = new Date().getFullYear() - new Date(2015, 1, 5).getFullYear();
+    var yearsOfExperience = new Date().getFullYear() - new Date(2015, 1, 5).getFullYear();
     var ctx = $("#developmentChart");
 
     myPieChart = new Chart(ctx,{
         type: 'pie',
         data: {
             datasets: [{
-                label: '# of years',
-                data: [2.5, 1, 0.6],
+                label: yearsOfExperience + ' of years',
+                data: [3, 2, 0.6],
                 backgroundColor: [
                     'rgba(20, 155, 32, 0.8)',
                     'rgba(54, 162, 235, 0.8)',
@@ -37,7 +38,7 @@ function createChart() {
         options: {
             title: {
                 display: true,
-                text: yearsOfExperiance + ' years experience',
+                text: yearsOfExperience + ' years experience',
                 fontColor: 'rgb(3, 3, 3)',
                 fontStyle: 'bold',
                 fontSize: 18
@@ -56,28 +57,111 @@ function createChart() {
     var theme = localStorage.getItem('theme');
 
     if (theme == 'dark') {
-        chartSwitchLightOff();
+        pieChartSwitchLightOff();
     }
 
     $("#light-off").click(function() { 
-        chartSwitchLightOff();
+        pieChartSwitchLightOff();
     });
     
     $("#light-on").click(function() { 
-        chartSwitchLightOn();
+        pieChartSwitchLightOn();
     });
 
     return myPieChart;
 }
 
-function chartSwitchLightOn() {
+function createStackChart() {
+    var yearsOfExperience = new Date().getFullYear() - new Date(2015, 1, 5).getFullYear();
+    var ctx = $("#developmentStackChart");
+
+    linearChart = new Chart(ctx,{
+        type: 'bar',
+        data: {
+            datasets: [{
+                label: yearsOfExperience + ' of years',
+                data: [3, 5],
+                backgroundColor: [
+                    'rgba(20, 155, 32, 0.8)',
+                    'rgba(54, 162, 235, 0.8)'
+                ],
+                borderColor: [
+                    'rgba(255,255,255,1)',
+                    'rgba(255,255,255,1)'
+                ],
+                borderWidth: 1
+            }],
+        
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: [
+                'Front-end',
+                'Back-end'
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: yearsOfExperience + ' years experience',
+                fontColor: 'rgb(3, 3, 3)',
+                fontStyle: 'bold',
+                fontSize: 18
+            },
+            legend: {
+                display: true,
+                labels: {
+                    fontColor: 'rgb(3, 3, 3)'
+                },
+                position: 'left'
+            },
+            animation: { animateScale: true },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+    var theme = localStorage.getItem('theme');
+
+    if (theme == 'dark') {
+        barChartSwitchLightOff();
+    }
+
+    $("#light-off").click(function() { 
+        barChartSwitchLightOff();
+    });
+    
+    $("#light-on").click(function() { 
+        barChartSwitchLightOn();
+    });
+
+    return linearChart;
+}
+
+function pieChartSwitchLightOn() {
     myPieChart.options.legend.labels.fontColor = 'rgb(3, 3, 3)';
     myPieChart.options.title.fontColor = 'rgb(3, 3, 3)';
     myPieChart.update();
 }
 
-function chartSwitchLightOff() {
+function barChartSwitchLightOn() 
+{
+    linearChart.options.legend.labels.fontColor = 'rgb(3, 3, 3)';
+    linearChart.options.title.fontColor = 'rgb(3, 3, 3)';
+    linearChart.update();
+}
+
+function pieChartSwitchLightOff() {
     myPieChart.options.legend.labels.fontColor = 'rgb(255, 255, 255)';
     myPieChart.options.title.fontColor = 'rgb(255, 255, 255)';
-    myPieChart.update();
+    myPieChart.update();  
+}
+
+function barChartSwitchLightOff() {
+    linearChart.options.legend.labels.fontColor = 'rgb(255, 255, 255)';
+    linearChart.options.title.fontColor = 'rgb(255, 255, 255)';
+    linearChart.update();    
 }
