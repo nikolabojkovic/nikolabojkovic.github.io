357ae8f7f8e8aa85bf7ac9db79d5547144634707
DESKTOP-ATL3G2E\Mare <markomarefudbal@gmail.com>
2022-05-24 12:25:26 +0200

    Created Project, set SCSS, font-awesome, Bootstrap, ngx-translater

---
+++ portfolio/Portfolio/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(library: FaIconLibrary) {       // Allowing icons to be used in angular
+    library.addIconPacks(fas,far);
+  }
+}
