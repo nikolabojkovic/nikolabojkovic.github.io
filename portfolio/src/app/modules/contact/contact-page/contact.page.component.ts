bcd46fb18277d04d37775fc867a7b21571d287f9
Nikola <nikolabojkovic@gmail.comm>
2022-07-06 17:38:36 +0200

    Portfolio-45 merge conflict resolved

--- portfolio/src/app/modules/contact/contact-page/contact.page.component.ts
+++ portfolio/src/app/modules/contact/contact-page/contact.page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { HeaderService } from "src/app/shared/services/header.service";
 import { SettingsService } from "src/app/shared/services/settings.service";
 import { ContactService } from "./contact.service";
+import { faFacebookF,faInstagram,faTwitter, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
     selector: "app-contact",
@@ -9,6 +10,10 @@ import { ContactService } from "./contact.service";
     styleUrls: ["./contact.page.component.scss"]
 })
 export class ContactPageComponent {
+    facebook =  faFacebookF;
+    instagram = faInstagram;
+    twitter = faTwitter;
+    linkedin = faLinkedinIn;
     constructor(private contactService: ContactService, private settingsService: SettingsService, private headerService: HeaderService) {
 
     }
